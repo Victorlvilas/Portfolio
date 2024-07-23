4b27f3a22a2c2483dc6d86a84fd1485b65ed914f
Victor <victorlvilas@gmail.com>
2024-07-23 13:53:53 +0200

    Pagina web completa primera revisión

--- js/index.js
+++ js/index.js
@@ -1,5 +1,29 @@
-window.addEventListener("load", function() {
-    document.getElementById("enviar").addEventListener("click",function(){
-        alert("Su mensaje ha sido enviado");
-    })
-})
+function mostrarDatos(event) {
+    event.preventDefault(); // Previene el envío del formulario
+    var nombre = document.getElementById('nombre').value;
+    var asunto = document.getElementById('asunto').value;
+    var mensaje = document.getElementById('mensaje').value;
+
+    var datos = "Nombre: " + nombre + "\n" + 
+                "Asunto: " + asunto + "\n" + 
+                "Mensaje: " + mensaje;
+
+    alert(datos);
+
+    setTimeout(function() {
+        location.reload();
+    }, 2000);
+
+}
+
+$(document).ready(function(){
+    $("#flip1").click(function(){
+      $("#panel1").slideToggle("slow");
+    });
+  });
+
+  $(document).ready(function(){
+    $("#flip2").click(function(){
+      $("#panel2").slideToggle("slow");
+    });
+  });
